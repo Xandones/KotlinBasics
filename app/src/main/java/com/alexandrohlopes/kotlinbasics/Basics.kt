3cb3493a7f6ec195557b7071d8797491f7d1e95d
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2022-09-27 09:56:48 -0300

    Difference bettween val and var

---
+++ app/src/main/java/com/alexandrohlopes/kotlinbasics/Basics.kt
@@ -0,0 +1,7 @@
+package com.alexandrohlopes.kotlinbasics
+
+fun main() {
+    val myName = "Alexandro" // Immutable variable - value
+    // myName = "João" -> This line will cause an error because we cannot change the value of a val only of a var
+    print("Yo people! My name is $myName!")
+}
