0a2eeaa2962b5c783552356a6abac12998c97ecc
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2022-09-27 16:18:42 -0300

    Displays maximum values for integer data types

--- app/src/main/java/com/alexandrohlopes/kotlinbasics/Basics.kt
+++ app/src/main/java/com/alexandrohlopes/kotlinbasics/Basics.kt
@@ -1,7 +1,19 @@
 package com.alexandrohlopes.kotlinbasics
 
 fun main() {
-    val myName = "Alexandro" // Immutable variable - value
+    val myName = "Alexandro" // Immutable string variable - value
+    var myAge = 40 // Mutable integer variable
     // myName = "João" -> This line will cause an error because we cannot change the value of a val only of a var
-    print("Yo people! My name is $myName!")
+    println("Yo people! My name is $myName and I'm $myAge years old.")
+
+    // Integer types
+    val maxByte : Byte = 127 // Size: 8 bits
+    val maxShort : Short = 32767 // Size: 16 bits
+    val maxInt : Int = 2147483647 // Size: 32 bits - this is the default set for integer numbers, that's why it's necessary to specify the type.
+    val maxLong : Long = 9223372036854775807 // Size: 64 bits
+    println("Maximum byte value: $maxByte")
+    println("Maximum short value: $maxShort")
+    println("Maximum integer (32 bits) value: $maxInt")
+    println("Maximum long value: $maxLong")
+
 }
